9382c8cfe0efe74b6c091bcd0cdaa329c621ee1e
HardikThummar97 <hardikthummar97@gmail.com>
2021-08-03 22:08:42 +0530

    Added home page with search functionlity and debouncing feature

---
+++ components/navbar.js
@@ -0,0 +1,16 @@
+function navbar() {
+  return `<div id="navbar">
+      <h3><a href="./index.html">Home</a></h3>
+
+      <div class="options">
+        <h3><a href="./recipeOfTheDay.html">Recipe of the day</a></h3>
+        <h3><a href="./latestRecipe.html">Latest recipe</a></h3>
+        <h3><a href="#">Register</a></h3>
+        <h3><a href="#">Login</a></h3>
+      </div>
+    </div>
+    
+    <div id="data"></div>`;
+}
+
+export default navbar;
